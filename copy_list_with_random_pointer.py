df26db7493c93e342c8c4c49c58f51c80bc2b4ab
Xiang Zuo <xiangzuo2012@gmail.com>
2016-04-07 23:22:02 -0700

    d

--- copy_list_with_random_pointer.py
+++ copy_list_with_random_pointer.py
@@ -53,6 +53,7 @@ class Solution:
 
 """
 九章solution, 思路非常清晰，比起hashmap没有耗费过多的空间
+O(1) space
 """
 class Solution:
     # @param head, a RandomListNode
@@ -119,8 +120,26 @@ class Solution:
         return dummy.next
 
 
-
-
+"""
+hash map soluton, O(n) and O(n) space
+"""
+class Solution:
+    # @param head, a RandomListNode
+    # @return a RandomListNode
+    def copyRandomList(self, head):
+        nodeDict = dict()
+        dummy = RandomListNode(0)
+        pointer, newHead = head, dummy
+        while pointer:
+            newNode = RandomListNode(pointer.label)
+            nodeDict[pointer] = newHead.next = newNode
+            newHead, pointer = newHead.next, pointer.next
+        pointer, newHead = head, dummy.next
+        while pointer:
+            if pointer.random:
+                newHead.random = nodeDict[pointer.random]
+            pointer, newHead = pointer.next, newHead.next
+        return dummy.next
 
 
 
